d562d32fe19d0d70300f7b006c0f7558a4f7db83
Tomas Zarate <tzarate@iupik>
2024-03-16 11:44:49 -0300

    postgres with docker and typeorm initial config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      migrations: ['dist/db/migrations/*{.ts,.js}'],
+      synchronize: true,
+    })
+  ],
   controllers: [],
   providers: [],
 })
